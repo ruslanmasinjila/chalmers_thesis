b399f729f449da93771827e8c5c68b626c2e4b3b
Ruslan Masinjila <ruslanmasinjila@gmail.com>
2019-03-28 15:04:26 +0100

    implemented threaded python server

--- py_node sockets/server.py
+++ py_node sockets/server.py
@@ -1,4 +1,5 @@
 import socket
+from threading import Thread
 
 # Ports used
 # 65432 is for listening to mmWave Visualizer
@@ -8,22 +9,29 @@ class Server():
 
     def __init__(self):
         
-        self.HOST = '127.0.0.1' # Standard loopback interface address (localhost)
-        self.PORT = 65432       # Port to listen on (non-privileged ports are > 1023)
+        self.HOST = '127.0.0.1'
+        self.visualizerPORT = 65432
+        self.fromJavaGUIPORT = 65431
         self.DIR=r"C:\chalmers_thesis\data"
         self.frameNum=1
-        self.startVisualizerServer()
+        self.launchThreads()
+
+
+    # For launching two servers
+    def launchThreads(self):
+        Thread(target = self.startVisualizerServer).start()
+        Thread(target = self.startFromJavaGUIServer).start()
 
 
     def startVisualizerServer(self):
-        file = open("C:\\chalmers_thesis\\data\\launched.txt",'w')
+        file = open("C:\\chalmers_thesis\\data\\visualizerServer.txt",'w')
         file.write("server launched")
         file.close()
         print("Server Launched")
         
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            print("Listening to port:", self.PORT)
-            s.bind((self.HOST, self.PORT))
+            print("Listening to port:", self.visualizerPORT)
+            s.bind((self.HOST, self.visualizerPORT))
             s.listen()
             conn, addr = s.accept()
             with conn:
@@ -42,5 +50,34 @@ class Server():
                     if not data:
                         break
 
+    def startFromJavaGUIServer(self):
+        file = open("C:\\chalmers_thesis\\data\\fromJavaGUIServer.txt",'w')
+        file.write("server launched")
+        file.close()
+        print("Server Launched")
+        
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            print("Listening to port:", self.fromJavaGUIPORT)
+            s.bind((self.HOST, self.fromJavaGUIPORT))
+            s.listen()
+            conn, addr = s.accept()
+            with conn:
+                print('Connected by', addr)
+                while True:
+                    try:
+                        data = conn.recv(65536)
+                    except:
+                        print("Crashed...")
+                        break
+                    file = open( self.DIR + "frame" + str(self.frameNum) + '.txt', 'w' )
+                    file.write(data.decode('utf-8'))
+                    file.close()
+                    print(data.decode('utf-8'))
+                    self.frameNum=self.frameNum+1
+                    if not data:
+                        break
+
+
 if __name__ == "__main__":
+
     server = Server()

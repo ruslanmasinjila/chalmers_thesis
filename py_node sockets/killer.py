72c49e842760d189969ad994768a6ffe61ec230c
Ruslan Masinjila <ruslanmasinjila@gmail.com>
2019-03-31 04:02:43 +0200

    Fixed environment dependancies and added magic formula

--- py_node sockets/killer.py
+++ py_node sockets/killer.py
@@ -9,5 +9,8 @@ for proc in process_iter():
     for conns in proc.connections(kind='inet'):
         for i in allPorts:
             if conns.laddr.port == i:
-                proc.send_signal(SIGTERM) # or SIGKILL
+                try:
+                    proc.send_signal(SIGTERM) # or SIGKILL
+                except:
+                    pass
                 continue 

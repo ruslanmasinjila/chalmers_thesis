7b4301938cb5edcb8e77e529ed6c5c92a968d410
Ruslan Masinjila <ruslanmasinjila@gmail.com>
2019-05-01 11:21:44 +0200

    ready for demo

--- py_node sockets/client.js
+++
@@ -1,4 +0,0 @@
-var net = require('net');
-var client = new net.Socket();
-client.connect(65432, '127.0.0.1')
-client.write("Hello World");
--- py_node sockets/cnn.py
+++ py_node sockets/cnn.py
@@ -1,7 +1,7 @@
 # CNN-LTSM Neural Network
 import tensorflow as tf
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten, Conv2D, MaxPooling2D, TimeDistributed, LSTM
+from tensorflow.keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPooling2D, TimeDistributed, LSTM
 import numpy as np
 import os
 
